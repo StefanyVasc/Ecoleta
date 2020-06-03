548a8d50895d4cf3827a2809f350694ef194a381
Stefany Sá <stefany.vasc.sa@gmail.com>
2020-06-02 22:05:08 -0300

    🚧 file that takes care of the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("listagem de usuários");
+app.use(express.json());
+app.use(routes);
 
-  response.json(["Stefany", "Babi", "Henrique", "Cleide"]);
-});
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
